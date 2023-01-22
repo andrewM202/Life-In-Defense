35c8f9fa1d14fdffc70c84a0213d4e53db3d2494
Hulitaphy <hulitaphy@gmail.com>
2023-01-21 20:08:20 -0500

    Drawing basic ground on screen

--- code/settings.py
+++ code/settings.py
@@ -2,5 +2,16 @@
 
 from pygame.math import Vector2
 
-Screen_Width, Screen_Height = 1280, 720
+# Screen height, width
+Screen_Width, Screen_Height = 1280, 768
+# Pixel size of each tile
 Tile_Size = 32
+
+# How many tiles fill up the screen
+Screen_Tile_Width  = Screen_Width / Tile_Size
+Screen_Tile_Height = Screen_Height / Tile_Size
+
+# How many tiles are in a chunk
+Chunk_Tile_Width, Chunk_Tile_Height = Screen_Tile_Width, Screen_Tile_Height
+
+
--- code/world.py
+++ code/world.py
@@ -1,4 +1,5 @@
 import pygame
+from settings import *
 
 class World:
     def __init__(self):
@@ -6,9 +7,23 @@ class World:
         # Get display surface
         self.display_surface = pygame.display.get_surface()
 
+    def perlin_noise(self, x, y):
+        """ Generate a value between 0 and 1.0 
+        for perlin noise """
+
     def load_world(self):
         """ Load all the tiles in the world in """
         # Load a block in
+        ground_center = pygame.image.load("../graphics/ground/ground_center.png").convert_alpha()
+        ground_top = pygame.image.load("../graphics/ground/ground_top.png").convert_alpha()
+
+        # Loop through entire screen and fill with tiles
+        for x in range(0, int(Screen_Tile_Width)):
+            for y in range(0, int(Screen_Tile_Height)):
+                if y == int(Screen_Tile_Height * 0.8):
+                    self.display_surface.blit(ground_top, (x * Tile_Size , y * Tile_Size))
+                elif y >= int(Screen_Tile_Height * 0.8):
+                    self.display_surface.blit(ground_center, (x * Tile_Size , y * Tile_Size))
 
     def run(self, dt):
         # Set background color 
